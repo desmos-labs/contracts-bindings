435485af1011eabb9d4b24a2d9ec438776a272a6
Manuel Turetta <manuel.turetta94@gmail.com>
2022-04-04 16:59:36 +0100

    doc: documentation for the relationships module

--- src/relationships/mock.rs
+++ src/relationships/mock.rs
@@ -1,3 +1,6 @@
+//! Contains some useful mocks of the Desmos x/relationships
+//! module's types ready made to be used in any test.
+
 use crate::relationships::{
     models::{Relationship, UserBlock},
     models_query::{QueryBlocksResponse, QueryRelationshipsResponse},
@@ -5,14 +8,12 @@ use crate::relationships::{
 };
 use cosmwasm_std::{to_binary, Addr, Binary, ContractResult, Uint64};
 
-/**
-This file contains some useful mocks of the Desmos x/relationships module's types ready made to be used
-in any test
- **/
-
+/// Struct that contains some utility methods to mock data of the Desmos
+/// x/relationships module.
 pub struct MockRelationshipsQueries {}
 
 impl MockRelationshipsQueries {
+    /// Get a mocked [`Relationship`].
     pub fn get_mock_relationship() -> Relationship {
         Relationship {
             creator: Addr::unchecked("desmos1nwp8gxrnmrsrzjdhvk47vvmthzxjtphgxp5ftc"),
@@ -21,6 +22,7 @@ impl MockRelationshipsQueries {
         }
     }
 
+    /// Get a mocked [`UserBlock`].
     pub fn get_mock_user_block() -> UserBlock {
         UserBlock {
             blocker: Addr::unchecked("desmos1nwp8gxrnmrsrzjdhvk47vvmthzxjtphgxp5ftc"),
@@ -31,28 +33,25 @@ impl MockRelationshipsQueries {
     }
 }
 
-pub struct MockRelationshipsQuerier {}
-
-impl MockRelationshipsQuerier {
-    pub fn query(query: &RelationshipsQuery) -> ContractResult<Binary> {
-        let response = match query {
-            RelationshipsQuery::Relationships { .. } => {
-                let relationship = MockRelationshipsQueries::get_mock_relationship();
-                to_binary(&QueryRelationshipsResponse {
-                    relationships: vec![relationship],
-                    pagination: Default::default(),
-                })
-            }
-            RelationshipsQuery::Blocks { .. } => {
-                let block = MockRelationshipsQueries::get_mock_user_block();
-                to_binary(&QueryBlocksResponse {
-                    blocks: vec![block],
-                    pagination: Default::default(),
-                })
-            }
-        };
-        response.into()
-    }
+/// Functions that mocks the relationships query responses.
+pub fn mock_relationships_query_response(query: &RelationshipsQuery) -> ContractResult<Binary> {
+    let response = match query {
+        RelationshipsQuery::Relationships { .. } => {
+            let relationship = MockRelationshipsQueries::get_mock_relationship();
+            to_binary(&QueryRelationshipsResponse {
+                relationships: vec![relationship],
+                pagination: Default::default(),
+            })
+        }
+        RelationshipsQuery::Blocks { .. } => {
+            let block = MockRelationshipsQueries::get_mock_user_block();
+            to_binary(&QueryBlocksResponse {
+                blocks: vec![block],
+                pagination: Default::default(),
+            })
+        }
+    };
+    response.into()
 }
 
 #[cfg(test)]
@@ -72,7 +71,7 @@ mod tests {
             subspace_id: Uint64::new(1),
             pagination: Default::default(),
         };
-        let response = MockRelationshipsQuerier::query(&query);
+        let response = MockRelationshipsQuerier::mock_relationships_query_response(&query);
         let expected = to_binary(&QueryRelationshipsResponse {
             relationships: vec![MockRelationshipsQueries::get_mock_relationship()],
             pagination: Default::default(),
@@ -88,7 +87,7 @@ mod tests {
             subspace_id: Uint64::new(1),
             pagination: Default::default(),
         };
-        let response = MockRelationshipsQuerier::query(&query);
+        let response = MockRelationshipsQuerier::mock_relationships_query_response(&query);
         let expected = to_binary(&QueryBlocksResponse {
             blocks: vec![MockRelationshipsQueries::get_mock_user_block()],
             pagination: Default::default(),
--- src/relationships/query.rs
+++ src/relationships/query.rs
@@ -1,21 +1,37 @@
+//! Contains the query messages that can be sent to the chain in order to query data relative
+//! to the x/relationships module.
+
 use crate::types::PageRequest;
 use cosmwasm_std::{Addr, Uint64};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+/// Query messages that can be sent to the x/relationships module.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum RelationshipsQuery {
+    /// Message to query the relationships inside a subspaces.
     Relationships {
+        /// Subspace to query the relationships for.
         subspace_id: Uint64,
+        /// Optional address of the user for which to query the relationships.
         user: Option<Addr>,
+        /// Optional address of the counterparty of the relationships (used only if the
+        /// `user` is provided).
         counterparty: Option<Addr>,
+        /// Pagination configs.
         pagination: Option<PageRequest>,
     },
+    /// Message to query the blocks created inside a subspace.
     Blocks {
+        /// Subspace to query the blocks for.
         subspace_id: Uint64,
+        /// Optional address of the blocker to query the blocks for.
         blocker: Option<Addr>,
+        /// Optional address of the blocked user to query the block for (used only if
+        /// the `blocker` is provided).
         blocked: Option<Addr>,
+        /// Pagination configs.
         pagination: Option<PageRequest>,
     },
 }
