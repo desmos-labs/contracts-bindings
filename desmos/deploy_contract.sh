81ba39cc9bfe7f36582acc053fceab2c93fabb94
Manuel <manuel.turetta94@gmail.com>
2022-05-10 15:18:47 +0200

    updated upload contract script

--- desmos/deploy_contract.sh
+++ desmos/deploy_contract.sh
@@ -3,9 +3,11 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DESMOS_HOME="$SCRIPT_DIR/.desmos"
 KEYRING_PASS=pass1234
-TEST_USER=user1
-TEST_USER_MNEMONIC="math track fish reopen project latin radio spoon please table between install cheap smile deer glide desk license bench vapor chef sock list case"
-TEST_USER_ADDRESS=desmos1jnpfa06xhflyjh6klwlrq8mk55s53czh6ncdm3
+# Smart contract dir
+SMART_CONTRACT="$SCRIPT_DIR/../artifacts/test_contract.wasm"
+# User 1 informations
+USER1=user1
+USER1_ADDRESS=desmos1jnpfa06xhflyjh6klwlrq8mk55s53czh6ncdm3
 
 
 
@@ -13,3 +15,21 @@ desmos() {
 	"$SCRIPT_DIR/desmos" --home="$DESMOS_HOME" "$@"
 }
 
+# Force the script to exit at the first error
+set -e
+
+# Upload the smart contract
+echo "Uploading contract..."
+echo $KEYRING_PASS | desmos tx wasm store "$SMART_CONTRACT" \
+  --from $USER1 --chain-id=testchain --keyring-backend=file -y --gas 20000000 \
+  -b=block
+
+# Initialize the contract
+echo "Initializing contract..."
+echo $KEYRING_PASS | desmos --keyring-backend=file tx wasm instantiate 1 "{}" \
+  --from $USER1 --label "test-contract" --admin $USER1_ADDRESS --chain-id=testchain -b=block -y
+echo "Contract initialized"
+
+# Print contract address
+CONTRACT=$(desmos query wasm list-contract-by-code 1 --output json | jq -r '.contracts[-1]')
+echo "Contract address $CONTRACT"
