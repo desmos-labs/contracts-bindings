a3113630a41f77386b91545eb165a0a67f9a89c5
Paul <p22626262@gmail.com>
2022-07-28 14:50:10 +0800

    feat: added failing desmos module

--- packages/bindings/src/test_utils.rs
+++ packages/bindings/src/test_utils.rs
@@ -4,14 +4,14 @@ use anyhow::Result as AnyResult;
 use cosmwasm_std::testing::{MockApi, MockStorage};
 use cosmwasm_std::{Addr, Api, Binary, BlockInfo, Empty, Querier, Storage};
 use cw_multi_test::{
-    App, AppResponse, BankKeeper, BasicAppBuilder, CosmosRouter, FailingDistribution,
+    App, AppResponse, BankKeeper, BasicApp, BasicAppBuilder, CosmosRouter, FailingDistribution,
     FailingStaking, Module, Router, WasmKeeper,
 };
 
 #[cfg(feature = "posts")]
 use crate::posts::mocks::mock_posts_query_response;
 #[cfg(feature = "profiles")]
-use crate::profiles::mocks::mock_profiles_query_response;
+use crate::profiles::{mocks::mock_profiles_query_response, msg::ProfilesMsg};
 #[cfg(feature = "reactions")]
 use crate::reactions::mocks::mock_reactions_query_response;
 #[cfg(feature = "relationships")]
@@ -19,7 +19,7 @@ use crate::relationships::mocks::mock_relationships_query_response;
 #[cfg(feature = "reports")]
 use crate::reports::mocks::mock_reports_query_response;
 #[cfg(feature = "subspaces")]
-use crate::subspaces::mocks::mock_subspaces_query_response;
+use crate::subspaces::{mocks::mock_subspaces_query_response, msg::SubspacesMsg};
 
 /// DesmosApp wraps the desmos custom module into a mock app for integration tests.
 pub type DesmosApp =
@@ -33,6 +33,41 @@ impl DesmosKeeper {
     /// Returns a new [DesmosKeeper].
     pub fn new() -> Self {
         DesmosKeeper {}
+    }
+
+    fn handle_profiles_msg(&self, msg: ProfilesMsg) -> AnyResult<AppResponse> {
+        match msg {
+            ProfilesMsg::SaveProfile { .. } => AnyResult::Ok(AppResponse::default()),
+            ProfilesMsg::DeleteProfile { .. } => AnyResult::Ok(AppResponse::default()),
+            ProfilesMsg::RequestDtagTransfer { .. } => AnyResult::Ok(AppResponse::default()),
+            ProfilesMsg::AcceptDtagTransferRequest { .. } => AnyResult::Ok(AppResponse::default()),
+            ProfilesMsg::RefuseDtagTransferRequest { .. } => AnyResult::Ok(AppResponse::default()),
+            ProfilesMsg::CancelDtagTransferRequest { .. } => AnyResult::Ok(AppResponse::default()),
+            ProfilesMsg::LinkChainAccount { .. } => AnyResult::Ok(AppResponse::default()),
+            ProfilesMsg::UnlinkChainAccount { .. } => AnyResult::Ok(AppResponse::default()),
+            ProfilesMsg::LinkApplication { .. } => AnyResult::Ok(AppResponse::default()),
+            ProfilesMsg::UnlinkApplication { .. } => AnyResult::Ok(AppResponse::default()),
+        }
+    }
+
+    fn handle_subspaces_msg(&self, msg: SubspacesMsg) -> AnyResult<AppResponse> {
+        match msg {
+            SubspacesMsg::CreateSubspace { .. } => AnyResult::Ok(AppResponse::default()),
+            SubspacesMsg::EditSubspace { .. } => AnyResult::Ok(AppResponse::default()),
+            SubspacesMsg::DeleteSubspace { .. } => AnyResult::Ok(AppResponse::default()),
+            SubspacesMsg::CreateSection { .. } => AnyResult::Ok(AppResponse::default()),
+            SubspacesMsg::EditSection { .. } => AnyResult::Ok(AppResponse::default()),
+            SubspacesMsg::MoveSection { .. } => AnyResult::Ok(AppResponse::default()),
+            SubspacesMsg::DeleteSection { .. } => AnyResult::Ok(AppResponse::default()),
+            SubspacesMsg::CreateUserGroup { .. } => AnyResult::Ok(AppResponse::default()),
+            SubspacesMsg::EditUserGroup { .. } => AnyResult::Ok(AppResponse::default()),
+            SubspacesMsg::MoveUserGroup { .. } => AnyResult::Ok(AppResponse::default()),
+            SubspacesMsg::SetUserGroupPermissions { .. } => AnyResult::Ok(AppResponse::default()),
+            SubspacesMsg::DeleteUserGroup { .. } => AnyResult::Ok(AppResponse::default()),
+            SubspacesMsg::AddUserToUserGroup { .. } => AnyResult::Ok(AppResponse::default()),
+            SubspacesMsg::RemoveUserFromUserGroup { .. } => AnyResult::Ok(AppResponse::default()),
+            SubspacesMsg::SetUserPermissions { .. } => AnyResult::Ok(AppResponse::default()),
+        }
     }
 }
 
@@ -51,8 +86,8 @@ impl Module for DesmosKeeper {
         msg: DesmosMsg,
     ) -> AnyResult<AppResponse> {
         match msg {
-            DesmosMsg::Profiles(_) => AnyResult::Ok(AppResponse::default()),
-            DesmosMsg::Subspaces(_) => AnyResult::Ok(AppResponse::default()),
+            DesmosMsg::Profiles(msg) => self.handle_profiles_msg(msg),
+            DesmosMsg::Subspaces(msg) => self.handle_subspaces_msg(msg),
             DesmosMsg::Relationships(_) => AnyResult::Ok(AppResponse::default()),
             DesmosMsg::Posts(_) => AnyResult::Ok(AppResponse::default()),
             DesmosMsg::Reports(_) => AnyResult::Ok(AppResponse::default()),
@@ -126,7 +161,12 @@ where
         .build(init_fn)
 }
 
-/// Returns a mock desmos app
+/// Returns a mock app with failing desmos module.
+pub fn mock_failing_desmos_app() -> BasicApp<DesmosMsg, DesmosQuery> {
+    BasicAppBuilder::<DesmosMsg, DesmosQuery>::new_custom().build(|_, _, _| {})
+}
+
+/// Returns a mock desmos app.
 pub fn mock_desmos_app() -> DesmosApp {
     BasicAppBuilder::<DesmosMsg, DesmosQuery>::new_custom()
         .with_custom(DesmosKeeper::new())
@@ -315,5 +355,24 @@ mod tests {
             report: get_mocked_report(&1u64.into()),
         };
         assert_eq!(expected, response)
+    }
+
+    #[test]
+    fn failing_app_excute_error() {
+        let mut app = mock_failing_desmos_app();
+        let result = app.execute(
+            Addr::unchecked(SENDER),
+            DesmosMsg::Profiles(ProfilesMsg::delete_profile(Addr::unchecked(SENDER))).into(),
+        );
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn failing_app_query_error() {
+        let app = mock_failing_desmos_app();
+        let app_querier = app.wrap();
+        let querier = ProfilesQuerier::new(app_querier.deref());
+        let result = querier.query_profile(Addr::unchecked(""));
+        assert!(result.is_err())
     }
 }
