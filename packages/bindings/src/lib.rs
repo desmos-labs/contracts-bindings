40f6a4063e5cb7d06f4988fc18548f2f81646a60
Paul <p22626262@gmail.com>
2022-08-03 17:18:43 +0800

    fix: re added mocks feature

--- packages/bindings/src/lib.rs
+++ packages/bindings/src/lib.rs
@@ -6,7 +6,7 @@
 extern crate core;
 #[cfg(feature = "iterators")]
 pub mod iter;
-#[cfg(not(target_arch = "wasm32"))]
+#[cfg(all(not(target_arch = "wasm32"), feature = "mocks"))]
 pub mod mocks;
 #[cfg(feature = "msg")]
 pub mod msg;
