8a7526172e5701cb8a465cc65d12585740b46356
Paul <p22626262@gmail.com>
2022-08-08 17:39:13 +0800

    fix: added back mocks features

--- packages/bindings/src/reports/mod.rs
+++ packages/bindings/src/reports/mod.rs
@@ -1,6 +1,6 @@
 //! Contains utilities,structs and enum to interact with the Desmos x/reports module.
 
-#[cfg(not(target_arch = "wasm32"))]
+#[cfg(all(not(target_arch = "wasm32"), feature = "mocks"))]
 pub mod mocks;
 pub mod models;
 #[cfg(feature = "query")]
