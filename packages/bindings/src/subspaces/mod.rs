eb0e635c7620b64dadd644b597a66d513bbcf406
Paul <p22626262@gmail.com>
2022-10-28 20:08:18 +0800

    feat: support subspaces proto

--- packages/bindings/src/subspaces/mod.rs
+++ packages/bindings/src/subspaces/mod.rs
@@ -1,13 +1,13 @@
 //! Contains utilities,structs and enums to interact with the Desmos x/subspaces module.
 
-#[cfg(all(not(target_arch = "wasm32"), feature = "query"))]
-pub mod mocks;
-
-pub mod models;
+#[cfg(feature = "msg")]
 pub mod msg;
 #[cfg(feature = "query")]
 pub mod querier;
 #[cfg(feature = "query")]
-pub mod query;
 
-pub mod models_query;
+pub mod types;
+
+pub mod proto {
+    pub use desmos_std::proto::desmos::subspaces::v3::*;
+}
