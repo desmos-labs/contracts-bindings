39ad2b1176ea270bbb46b56028a0d03e18af5ee3
Paul <p22626262@gmail.com>
2023-02-24 19:20:16 +0800

    chore: remove unused imports

--- packages/bindings/src/types.rs
+++ packages/bindings/src/types.rs
@@ -4,6 +4,3 @@ pub use desmos_std::proto::cosmos::base::query::v1beta1::PageRequest;
 pub use desmos_std::proto::ibc::core::client::v1::Height;
 pub use desmos_std::public_keys::{Ed25519PublicKey, Secp256k1PublicKey, Secp256r1PublicKey};
 pub use desmos_std::shim::Any;
-
-use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{Binary, Uint64};
